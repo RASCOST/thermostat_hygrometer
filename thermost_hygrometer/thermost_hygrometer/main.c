4b53ada98367867a5feb2a38d2e39a738aff73ec
RASCOST <ra.silva.dcosta@gmail.com>
2022-12-10 12:04:02 +0100

    firmware - added timer0 setup, start, stop, and interrupt

--- thermost_hygrometer/thermost_hygrometer/main.c
+++ thermost_hygrometer/thermost_hygrometer/main.c
@@ -5,4 +5,48 @@
  * Author : rasc
  */ 
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+volatile unsigned char tick = 0;
+
+/***********
+   Timer 0
+ **********/
+void setupTimer0() {
+  TCCR0A |= (1 << WGM01); // Set CTC mode
+  OCR0A = 0x0F; // 1 us
+  TIMSK0 |= (1 << OCIE0A);// Interrupt compare
+  sei();
+}
+
+void startTimer0() {
+  TCCR0B |= (1 << CS00); // No prescaling
+}
+
+void stopTimer0() {
+  TCCR0B  |= (0 << CS00);
+}
+
+
+
+/***********************
+   Interrupts handlers
+ **********************/
+ISR(TIMER0_OVF_vect) {
+  tick++;
+}
+
+int main(void)
+{
+    /* Replace with your application code */
+	setupTimer0();
+	startTimer0();
+	
+    while (1) 
+    {
+		//readDHT11();
+    }
+}
 
