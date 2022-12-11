69c0273b25d981afb049acd9d465e0a6a8cb718b
RASCOST <ra.silva.dcosta@gmail.com>
2022-12-11 12:57:57 +0100

    firmware - initialized PORTC, created functions that change the DHT11 direction in PORTC. Changed the defined pins, and added the function initSystem. Added the lcd.h and lcd.c to the project

--- thermost_hygrometer/thermost_hygrometer/main.c
+++ thermost_hygrometer/thermost_hygrometer/main.c
@@ -2,18 +2,34 @@
  * thermost_hygrometer.c
  *
  * Created: 20/02/2022 10:51:47
- * Author : rasc
+ * Author : RASC
  */ 
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdlib.h>
+#include "lcd.h"
 
-#define DHT11 PC2
-#define EN PC0
-#define RS PC1
+#define DHT11 PINC2
+#define EN PINC0
+#define RS PINC1
 
 volatile unsigned char tick = 0;
+
+/**********
+   PORTC
+**********/
+void initPORTC(void) {
+	DDRC = 0x07; // EN, RS and DHT11 is output when start communication 
+}
+
+void setDTH11PortCIN(void) {
+	DDRC &= 0xFB
+}
+
+void setDTH11PortCOUT(void) {
+	DDRC = (DDRC & 0xFB) | (DDRC | 0x04);
+}
 
 /***********
    Timer 0
@@ -57,14 +73,23 @@ ISR(TIMER0_OVF_vect) {
   tick++;
 }
 
+void initSystem(void) {
+	setupTimer0();
+	lcdInit();
+	lcdMoveCursor(0, 0);
+	lcdPrint("Thermostat");
+	lcdMoveCursor(1, 0);
+	lcdPrint("Thermostat");
+}
+
 int main(void)
 {
-	setupTimer0();
-	startTimer0();
+	initSystem();
+	//startTimer0();
 	
     while (1) 
     {
-		readDHT11();
+		//readDHT11();
     }
 }
 
