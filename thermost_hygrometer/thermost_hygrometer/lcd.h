c8f4f0df0dd1476975823df79fca9797b91c8e35
RASCOST <ra.silva.dcosta@gmail.com>
2022-12-17 14:57:49 +0100

    added the lcd.h and lcd.c

---
+++ thermost_hygrometer/thermost_hygrometer/lcd.h
@@ -0,0 +1,27 @@
+/*
+ * lcd.h
+ *
+ * Created: 15-03-2015 10:04:02
+ * Author: RASC
+ *
+ * Modified: 11-12-2002
+ *  - changed char to uint8_t in line 20
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+void lcdCmd( uint8_t cmd );
+void lcdData( uint8_t data );
+void lcdPrint( char *msg );
+void lcdMoveCursor( uint8_t line, uint8_t row );
+void lcdCursorLeftShift();
+void lcdClear();
+void lcdCursorBlink();
+void lcdClearCursor();
+void lcdInit( void );
+
+#endif /* LCD_H_ */
