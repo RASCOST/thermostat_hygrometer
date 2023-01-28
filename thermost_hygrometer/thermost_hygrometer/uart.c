ed3d03b930a456553a6ee7d6457b0e7b0dbe1e8f
RASCOST <ra.silva.dcosta@gmail.com>
2023-01-28 10:18:33 +0100

    refactored the txString, txMultiByteData, txByteData functions, added L in the macro UBRR_VALUE, added the UBRR_VALUE in the initUart function.

--- thermost_hygrometer/thermost_hygrometer/uart.c
+++ thermost_hygrometer/thermost_hygrometer/uart.c
@@ -1,32 +1,39 @@
 #include "uart.h"
 
-#define FOSC 800000UL
+#define FOSC 8000000
 #define BAUDRATE 19200
-#define UBRR_VALUE (FOSC/(16*BAUDRATE)) - 1
+#define UBRR_VAL ((FOSC/(16L*BAUDRATE))-1)
 
-#define IS_BUFF_EMPTY UCSR0A & ~(1 << UDRE0)
+#define IS_BUFF_EMPTY (UCSR0A & (1 << UDRE0))
 #define N_ELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 #define FALSE 0
 #define TRUE 1
 
-void initUART(uint16_t baudRate) {
-    UBRR0H = (uint8_t)(baudRate >> 8);
-    UBRR0L = (uint8_t)(baudRate);
+void initUART(void) {
+    UBRR0H = (uint8_t) (8 >> UBRR_VAL);
+    UBRR0L = (uint8_t) (UBRR_VAL);
     UCSR0B |= (1 << TXEN0); // only transmitter next version of file will include preprocessor directives to choose
     UCSR0C |= (1 << UCSZ00); // frame format: 8data, 1 stop bit
 }
 
 uint8_t txByteData(uint8_t data) {
-    if (IS_BUFF_EMPTY) {
-        UDRE0 = data;
-        return TRUE;
-    }
-    
-    return FALSE;
+    while (!IS_BUFF_EMPTY);
+    UDR0 = data;
+
 }
 
 void txMultiByteData(uint8_t* data, uint8_t length) {
     for (uint8_t idx = 0; idx < length; idx++) {
-        while (!txByteData(data[idx]));
+        //while (!txByteData(data[idx]));
+        txByteData(data[idx]);
+    }
+}
+
+void txString(char *str ) {
+    uint8_t idx = 0;
+    
+    while (str[idx] != '\0') {
+        txByteData(str[idx]);
+        idx++;
     }
 }
